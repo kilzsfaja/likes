dd9165d495f4303f26b07db4df64592b5faa48c2
kilzsfaja <kevin_k_gautier@hotmail.com>
2023-03-22 08:31:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var likes1 = document.querySelector("#likes1");
+var likes2 = document.querySelector("#likes2");
+var likes3 = document.querySelector("#likes3");
+
+function likes1Increase() {
+  likes1.innerText++;
+}
+
+function likes2Increase() {
+  likes2.innerText++;
+}
+
+function likes3Increase() {
+  likes3.innerText++;
+}
